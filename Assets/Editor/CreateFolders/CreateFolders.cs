8722956da741127dea0ee8b29b0881c42fdbae27
serj <13497925+gamedevserj@users.noreply.github.com>
2022-04-13 20:21:09 +0400

    Restructured project Added script to scale animation curves

--- Assets/Editor/CreateFolders/CreateFolders.cs
+++ Assets/Editor/CreateFolders/CreateFolders.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using System.IO;
+
 namespace UsefulUnityScripts
 {
     public class CreateFolders : EditorWindow
@@ -58,30 +59,6 @@ namespace UsefulUnityScripts
                 }
             }
             AssetDatabase.Refresh();
-        }
-    }
-
-    [System.Serializable]
-    public class Folder
-    {
-        public string folderName = "";
-        public Folder[] subFolders = new Folder[0];
-
-        public Folder()
-        {}
-        public Folder(string f)
-        {
-            folderName = f;
-        }
-
-        public Folder(string folderName, Folder[] subFolders)
-        {
-            this.folderName = folderName;
-            this.subFolders = new Folder[subFolders.Length];
-            for (int i = 0; i < subFolders.Length; i++)
-            {
-                this.subFolders[i] = new Folder(subFolders[i].folderName);
-            }
         }
     }
 }
---
+++ Assets/Editor/CreateFolders/Folder.cs
@@ -0,0 +1,26 @@
+﻿namespace UsefulUnityScripts
+{
+    [System.Serializable]
+    public class Folder
+    {
+        public string folderName = "";
+        public Folder[] subFolders = new Folder[0];
+
+        public Folder()
+        {}
+        public Folder(string f)
+        {
+            folderName = f;
+        }
+
+        public Folder(string folderName, Folder[] subFolders)
+        {
+            this.folderName = folderName;
+            this.subFolders = new Folder[subFolders.Length];
+            for (int i = 0; i < subFolders.Length; i++)
+            {
+                this.subFolders[i] = new Folder(subFolders[i].folderName);
+            }
+        }
+    }
+}
